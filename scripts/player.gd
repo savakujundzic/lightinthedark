a8ac042a073378f008210cf03a99f1a3804aed4a
kaqudneol <142493974+kaqudneol@users.noreply.github.com>
2024-07-17 14:33:23 +0200

    Implemented SFX and respawn
    -Switch sound [ On , Off ] when changing from light to dark
    -Checkpoints and a death area

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,20 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
 @onready var body = $"."
 
+@onready var checkpoint:Vector2
 const SPEED = 170.0
 const JUMP_VELOCITY = -250
 
+# Switch SFX
+@onready var offSFX = $Off
+@onready var onSFX = $On
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	checkpoint = body.global_transform.origin
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -40,3 +48,21 @@ func _on_main_coll_changed():
 		body.collision_mask = 2
 	elif current_mask == 2:
 		body.collision_mask = 1
+	
+	if current_mask == 1:
+		onSFX.play()
+	elif current_layer == 2: 
+		offSFX.play()
+	
+
+
+
+func _on_checkpoint_body_entered(body):
+	if body is Player:
+		checkpoint = body.global_transform.origin
+		print(checkpoint)
+
+func _on_death_area_body_entered(body):
+	if body is Player:
+		body.global_transform.origin = checkpoint 
+		print("DEATH")
