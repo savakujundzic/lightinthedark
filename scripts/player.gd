8ddf2e9d86ff141b1844a086fd48e49026283055
savakujundzic <kujundzicsava4@gmail.com>
2024-07-19 22:48:08 +0200

    character

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,20 @@
 class_name Player extends CharacterBody2D
 
 @onready var body = $"."
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
 
 @onready var checkpoint:Vector2
 const SPEED = 170.0
 const JUMP_VELOCITY = -250
-
+var animdirection = "left"
 # Switch SFX
 @onready var offSFX = $Off
 @onready var onSFX = $On
+
+# music
+@onready var lightmusic = $lightmusic
+@onready var darkmusic = $darkmusic
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -20,11 +26,9 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
@@ -32,8 +36,22 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	animations()
+
+func animations():
+	if velocity.x < 0: animdirection = "left"
+	elif velocity.x > 0: animdirection = "right"
+	elif velocity.y > 0: animdirection = "up"
+	if velocity.length() == 0:
+		animation_player.play("idle")
+	else:
+		if animdirection == "left":
+			sprite_2d.flip_h = true
+			animation_player.play("run")
+		elif animdirection == "right":
+			sprite_2d.flip_h = false
+			animation_player.play("run")
 
 func _on_main_coll_changed():
 	var current_layer = body.collision_layer
@@ -50,8 +68,12 @@ func _on_main_coll_changed():
 		body.collision_mask = 1
 	
 	if current_mask == 1:
+		darkmusic.stream_paused = true
+		lightmusic.stream_paused = false
 		onSFX.play()
 	elif current_layer == 2: 
+		darkmusic.stream_paused = false
+		lightmusic.stream_paused = true
 		offSFX.play()
 	
 
